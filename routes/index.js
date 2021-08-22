70986f3f1b37a612c11fc1d6c02e011173014664
Aya-Ayman <Aya.Ayman@ibm.com>
2021-08-22 23:39:45 +0200

    v2

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'The Book List' });
+  res.render('index', { title: 'The Book List v2' });
 });
 
 module.exports = router;
