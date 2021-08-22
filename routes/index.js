29d40783b3f7a170f6cb17943dfbce3d58ab0a74
Aya-Ayman <Aya.Ayman@ibm.com>
2021-08-23 00:24:29 +0200

    Revert "v2"
    This reverts commit 70986f3f1b37a612c11fc1d6c02e011173014664.

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'The Book List v2' });
+  res.render('index', { title: 'The Book List' });
 });
 
 module.exports = router;
